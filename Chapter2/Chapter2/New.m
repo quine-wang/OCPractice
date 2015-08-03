e1352759123e693199db4982827aa5a6aba0cb08
kunpeng.wang <kunpeng.wang@qunar.com>
2015-08-03 08:27:03 +0800

    chap2

---
+++ Chapter2/Chapter2/New.m
@@ -0,0 +1,13 @@
+//
+//  New.m
+//  Chapter2
+//
+//  Created by quine on 15/8/3.
+//  Copyright (c) 2015年 quine. All rights reserved.
+//
+
+#import "New.h"
+
+@implementation New
+
+@end
---
+++ Chapter2/Chapter2/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  Chapter2
+//
+//  Created by quine on 15/8/3.
+//  Copyright (c) 2015年 quine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // case 1
+        NSLog(@"Hello, World!");
+        // case 2
+        NSLog(@"Programming is Fun!");
+        NSLog(@"Programming in Objective-C is more fun!");
+        // case 3
+        NSLog(@"Testing...\n.1\n..2\n...3");
+        // case 4
+        
+        
+    }
+    return 0;
+}
